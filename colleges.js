e70415f026e75e2574ebde6a43e0762ed8f29694
Liz Abinante <eabinante@gmail.com>
2013-09-25 13:54:15 -0500

    states and colleges collections working and displaying

--- colleges.js
+++ colleges.js
@@ -1,19 +1,49 @@
+Datum = new Meteor.Collection("data");
+States = new Meteor.Collection("states");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to colleges.";
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.list.stateName = function() {
+    return States.find().fetch();
+  };
+
+  Template.list.events({
+    'click a' : function(event) {
+      var getState = $(this).attr('state');
+      event.preventDefault();
     }
   });
+
+  Meteor.subscribe("datum");
+  Meteor.subscribe("states");
+
+  return Datum.find().fetch();
+  return States.find().fetch();
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.publish("datum", function() {
+      return Datum.find();
+    });
+
+    Meteor.publish("states", function() {
+      return States.find();
+    });
+
+    Datum.allow({
+      insert: function () {
+        return false;
+      }
+    });
+
+    States.allow({
+      insert: function () {
+        return false;
+      }
+    });
   });
 }
