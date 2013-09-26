7020e2f8615f08f5ace27355f83537bfc7407ffd
Liz Abinante <eabinante@gmail.com>
2013-09-26 15:17:44 -0500

    some helpers

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,12 @@
 class School < ActiveRecord::Base
   STATES = School.all.map(&:state).uniq.sort
   RELIGIONS = School.all.map(&:religious).delete_if{|r|r==nil}.uniq.sort
+
+  def self.num_religions
+    RELIGIONS.length
+  end
+
+  def self.random_religion
+    RELIGIONS.sample
+  end
 end
